bfc028cf2d8473887dcf1e5cafb2cbd6f55d1e26
fabiom95 <fabiomar95@gmail.com>
2020-09-14 20:19:05 +0200

    registration
    added registration and login

---
+++ code/Travelgram/app/src/main/java/com/example/travelgram/RegisterActivity.java
@@ -0,0 +1,27 @@
+package com.example.travelgram;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        Button button = findViewById(R.id.loginBtn);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+}
