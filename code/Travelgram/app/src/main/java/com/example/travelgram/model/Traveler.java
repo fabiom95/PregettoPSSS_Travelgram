89199a841badf5ff92ff82f72f06790ed2527fb2
Fabrizio Guillaro <fabri.guillaro@gmail.com>
2020-09-18 11:50:41 +0200

    backup vpp

--- code/Travelgram/app/src/main/java/com/example/travelgram/model/Traveler.java
+++ code/Travelgram/app/src/main/java/com/example/travelgram/model/Traveler.java
@@ -1,4 +1,17 @@
 package com.example.travelgram.model;
 
 public class Traveler {
+
+    private String userName;
+    private String name;
+    private String lastName;
+    private String email;
+    private String password;
+    private String description; // string?
+    private String birthDate; // string o date?
+    //immagine del profilo
+
+
+
+
 }
