fcf63f280a86a91b2ecdce89805634ef61c3e405
Fabrizio Guillaro <fabri.guillaro@gmail.com>
2020-09-25 13:55:45 +0200

    aggiornamento mappa

--- code/Travelgram/app/src/main/java/com/psss/travelgram/view/activity/PlaceActivity.java
+++ code/Travelgram/app/src/main/java/com/psss/travelgram/view/activity/PlaceActivity.java
@@ -4,28 +4,28 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.psss.travelgram.R;
-import com.psss.travelgram.view.fragment.ScratchMapFragment;
 import com.psss.travelgram.SectionsPagerAdapter;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 import com.psss.travelgram.viewmodel.PlaceViewModel;
 
-import androidx.appcompat.widget.SwitchCompat;
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.CompoundButton;
-import android.widget.TextView;
+import android.view.View.OnClickListener;
 
 
-public class PlaceActivity extends AppCompatActivity {
+public class PlaceActivity extends AppCompatActivity implements OnClickListener {
 
-    public static final String COUNTRY_NAME = "com.psss.travelgram.COUNTRY_NAME";
     private String countryName;
     private PlaceViewModel placeViewModel;
+    private MenuItem visited;
+    private MenuItem wish;
 
 
     @Override
@@ -33,40 +33,26 @@ public class PlaceActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place);
 
+        // riceve il nome del paese
         Intent intent = getIntent();
-        countryName = intent.getStringExtra(ScratchMapFragment.COUNTRY_NAME);
+        countryName = intent.getStringExtra("countryName");
 
         // l'adapter istanzia le pagine da mostrare nelle varie sezioni
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
-
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
 
         // view model
         placeViewModel = new PlaceViewModel();
 
-        // switch visited
-        SwitchCompat visited = findViewById(R.id.visited);
-        visited.setOnCheckedChangeListener(
-                new CompoundButton.OnCheckedChangeListener() {
-                       @Override
-                       public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                           //TODO: controllare se effettivamente cambia
-                           placeViewModel.addVisitedCountry(countryName);
-                           setResult(1);    //0: country segnato come visited
-                           setIntent();
-                       }
-                   });
-
-
-                // Toolbar
-                Toolbar mToolbar = findViewById(R.id.toolbar);
+        // Toolbar
+        Toolbar mToolbar = findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(countryName);
-        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+        mToolbar.setNavigationOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 onBackPressed();
@@ -75,15 +61,78 @@ public class PlaceActivity extends AppCompatActivity {
 
         // bottone aggiungi memory
         FloatingActionButton addMemoryBtn = findViewById(R.id.addMemoryBtn);
+        addMemoryBtn.setOnClickListener(this);
 
-        addMemoryBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if(visited.isChecked())
+            placeViewModel.addVisitedCountry(countryName);
+        //else remove visited
+        // if wish...else
+
+        // calcolo risultato
+        Intent intent = new Intent();
+        intent.putExtra("countryName", countryName);
+        if(visited.isChecked())
+            setResult(2, intent);
+        else if (wish.isChecked())
+            setResult(3, intent);
+        else
+            setResult(1, intent);
+        super.onBackPressed();
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+
+            case R.id.addMemoryBtn:
                 Intent intent = new Intent(getApplicationContext(), InsertMemoryActivity.class);
                 intent.putExtra("countryName", countryName);
                 startActivity(intent);
-            }
-        });
+                break;
+
+            default:
+                break;
+        }
+    }
+
+
+    // per i bottoni della toolbar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.visited:
+                item.setChecked(!item.isChecked());
+                item.setIcon(item.isChecked() ? R.drawable.place_visited_checked : R.drawable.place_visited);
+                return true;
+
+            case R.id.wish:
+                item.setChecked(!item.isChecked());
+                item.setIcon(item.isChecked() ? R.drawable.place_wish_checked : R.drawable.place_wish);
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.place_menu, menu);
+        visited = menu.findItem(R.id.visited);
+        wish = menu.findItem(R.id.wish);
+
+        return true;
     }
 
 }
--- code/Travelgram/app/src/main/java/com/psss/travelgram/view/fragment/ScratchMapFragment.java
+++ code/Travelgram/app/src/main/java/com/psss/travelgram/view/fragment/ScratchMapFragment.java
@@ -7,15 +7,14 @@ import androidx.fragment.app.Fragment;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
+import com.google.maps.android.data.Layer;
+import com.google.maps.android.data.Layer.OnFeatureClickListener;
 import com.google.maps.android.data.geojson.GeoJsonFeature;
 import com.google.maps.android.data.geojson.GeoJsonLayer;
-import com.google.maps.android.data.geojson.GeoJsonPolygon;
 import com.google.maps.android.data.geojson.GeoJsonPolygonStyle;
 import com.psss.travelgram.view.activity.PlaceActivity;
 import com.psss.travelgram.viewmodel.ScratchMapViewModel;
@@ -23,123 +22,123 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 
 
 import com.psss.travelgram.R;
-import com.google.maps.android.data.kml.KmlLayer;
 import com.google.maps.android.data.Feature;
 
 import org.json.JSONException;
-import org.xmlpull.v1.XmlPullParserException;
-
-import java.io.IOException;
-
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Observable;
-import java.util.Observer;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
+import java.util.ArrayList;
 
 
-public class ScratchMapFragment extends Fragment implements OnMapReadyCallback {
+public class ScratchMapFragment extends Fragment implements OnMapReadyCallback, OnFeatureClickListener {
 
-    public static final String COUNTRY_NAME = "com.psss.travelgram.COUNTRY_NAME";
-    private static final String TAG = ScratchMapFragment.class.getSimpleName();
-    private GoogleMap scratchMap;
     private ScratchMapViewModel scratchMapViewModel;
     private GeoJsonLayer layer;
 
-
-    // aggiunge il livello KML
-    /*private void addLayer(){
-        try {
-            if (layer != null) layer.removeLayerFromMap();
-
-            layer = new KmlLayer(scratchMap, R.raw.world, getActivity().getApplicationContext());
-            layer.addLayerToMap();
-
-            layer.setOnFeatureClickListener(new KmlLayer.OnFeatureClickListener() {
-                @Override
-                public void onFeatureClick(Feature feature) {
-                    Intent intent = new Intent(getActivity(), PlaceActivity.class);
-                    intent.putExtra(COUNTRY_NAME, feature.getProperty("name"));
-                    startActivity(intent);
-                }
-            });
-
-        } catch (XmlPullParserException e) { e.printStackTrace();
-        } catch (IOException e) { e.printStackTrace();
-        }
-
-    }*/
-
-    // aggiunge il livello GeoJson
-    private void addLayer(){
-        try {
-            if (layer != null) layer.removeLayerFromMap();
-
-            layer = new GeoJsonLayer(scratchMap, R.raw.world_j, getActivity().getApplicationContext());
-            layer.addLayerToMap();
-
-            layer.setOnFeatureClickListener(new KmlLayer.OnFeatureClickListener() {
-                @Override
-                public void onFeatureClick(Feature feature) {
-                    /*GeoJsonFeature geometry = new GeoJsonFeature(feature.getGeometry(), null,null,null);
-
-                    GeoJsonPolygonStyle style = new GeoJsonPolygonStyle();
-                    style.setFillColor(Color.RED);
-
-                    geometry.setPolygonStyle(style);
-                    layer.addFeature(geometry);*/
-
-                    Intent intent = new Intent(getActivity(), PlaceActivity.class);
-                    intent.putExtra(COUNTRY_NAME, feature.getProperty("name"));
-                    startActivityForResult(intent,0);
-                }
-            });
-
-        } catch (JSONException e) { e.printStackTrace();
-        } catch (IOException e) { e.printStackTrace();
-        }
-
-    }
 
 
     @Override
     public void onMapReady(final GoogleMap scratchMap) {
 
-        this.scratchMap = scratchMap;
         scratchMap.setMinZoomPreference(3.0f);
         scratchMap.setMaxZoomPreference(6.0f);
 
-        // assegna lo stile
+        // stile della mappa di base
         scratchMap.setMapStyle( MapStyleOptions.loadRawResourceStyle(
                 getActivity().getApplicationContext(), R.raw.country_style));
 
         scratchMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(46,10), 3.5f));
 
-        addLayer();
+        try {
+            layer = new GeoJsonLayer(scratchMap, R.raw.world_j, getActivity().getApplicationContext());
+            layer.addLayerToMap();
 
+
+            ArrayList<String> countries = new ArrayList<>();
+            for (GeoJsonFeature feature : layer.getFeatures()) {
+                countries.add(feature.getProperty("name"));
+            }
+            try {
+                FileWriter myWriter = new FileWriter("C:\\Users\\fabri\\Desktop\\filename.txt");
+                for (GeoJsonFeature feature : layer.getFeatures())
+                    myWriter.write("<item>" + feature.getProperty("name") + "</item>");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            //getResources().getStringArray(R.array.countries);
+
+
+            // stile del livello
+            GeoJsonPolygonStyle style = layer.getDefaultPolygonStyle();
+            style.setFillColor(getResources().getColor(R.color.base));
+            style.setStrokeWidth(3);
+
+            layer.setOnFeatureClickListener(this);
+
+        } catch (JSONException e) { e.printStackTrace();
+        } catch (IOException e) { e.printStackTrace();
+        }
     }
 
+
+
+    // al click dello stato apre la PlaceActivity
+    @Override
+    public void onFeatureClick(Feature feature) {
+        Intent intent = new Intent(getActivity(), PlaceActivity.class);
+        intent.putExtra("countryName", feature.getProperty("name"));
+        startActivityForResult(intent,0);
+    }
+
+
+    // al termine della PlaceActivity può colorare lo stato
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //TODO: vedere se si può migliorare (pattern state, caricando dal database)
+        switch (resultCode){
+            case 1: // base
+                colorCountry(R.color.base, data.getStringExtra("countryName"));
+                break;
+            case 2: // visited
+                colorCountry(R.color.visited, data.getStringExtra("countryName"));
+                break;
+            case 3: // wish
+                colorCountry(R.color.wish, data.getStringExtra("countryName"));
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    // stile di uno specifico stato
+    public void colorCountry(int color, String countryName) {
+        GeoJsonPolygonStyle style = new GeoJsonPolygonStyle();
+        style.setFillColor(getResources().getColor(color));
+        style.setStrokeWidth(3);
+
+        for (GeoJsonFeature feature : layer.getFeatures()) {
+            if (feature.getProperty("name").equals(countryName)) {
+                feature.setPolygonStyle(style);
+                return;
+            }
+        }
+    }
+
+
+
+    // ---------------------------------------------------------------------------------------------
 
     @Nullable
     @Override
@@ -159,14 +158,5 @@ public class ScratchMapFragment extends Fragment implements OnMapReadyCallback {
         }
     }
 
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if(resultCode == 1){
-            Log.d("PROVA", "cambia colore");
-        }
-    }
 
 }
